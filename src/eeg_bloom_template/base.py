a8a102bd63e15a8ae079fdeadd37d80e351cb8ce
Cristopher Hernandez <22552070+CristopherH95@users.noreply.github.com>
2024-03-21 23:01:23 -0700

    Implement checker class for making comparisons

--- src/eeg_bloom_template/base.py
+++ src/eeg_bloom_template/base.py
@@ -8,6 +8,10 @@ class BaseEEGTemplateData(abc.ABC):
     Abstract base class defining the general structure of an EEG template data class.
     """
     bloom_filters: typing.List[rbloom.Bloom]
+    segment_ratio: float
 
-    def __init__(self, bloom_filters: typing.List[rbloom]):
+    def __init__(self, bloom_filters: typing.List[rbloom], segment_ratio: float):
+        if not 0 < segment_ratio <= 1:
+            raise ValueError(f'Segment ratio must be between 0 and 1, but got {segment_ratio}.')
         self.bloom_filters = bloom_filters
+        self.segment_ratio = segment_ratio
--- src/eeg_bloom_template/comparison.py
+++ src/eeg_bloom_template/comparison.py
@@ -1,10 +1,16 @@
 import typing
+import rbloom
 import numpy as np
 
 from .base import BaseEEGTemplateData
+from .utils import iter_ratio_slices
 
 
 class EEGTemplateDataChecker:
+    """
+    Class which implements comparison operations for EEG templates against EEG feature data vectors. A tolerance
+    value is used to indicate at which point the EEG data is to be considered a non-match for the template.
+    """
     def __init__(self, template: BaseEEGTemplateData, tolerance: float):
         if not 0 < tolerance <= 1:
             raise ValueError(f'Tolerance must be between 0 and 1 (got {tolerance}).')
@@ -12,4 +18,56 @@ class EEGTemplateDataChecker:
         self.tolerance = tolerance
 
     def check(self, eeg_data: typing.List[np.ndarray]) -> bool:
-        pass
+        """
+        Checks the given EEG feature data vectors to see if they are approximately a match for the template data.
+
+        :param eeg_data: A list of EEG feature data vectors to check.
+        :returns: A flag indicating whether the EEG feature data vectors are approximately a match.
+        """
+        hits = 0
+        iterations = 0
+        filter_idx = 0
+        number_of_filters = len(self.template.bloom_filters)
+
+        for segment in iter_ratio_slices(eeg_data, self.template.segment_ratio):
+            new_hits = self._check_segment_against_filter(segment, self.template.bloom_filters[filter_idx])
+            hits += new_hits
+            filter_idx = min(filter_idx + 1, number_of_filters)
+            iterations += 1
+
+        hit_ratio = hits / iterations
+        deviation_ratio = 1 - hit_ratio
+
+        return deviation_ratio <= self.tolerance
+
+    @classmethod
+    def _check_segment_against_filter(cls, data_segment: typing.List[np.ndarray], bloom_filter: rbloom.Bloom) -> int:
+        """
+        Accumulates the number of matching elements are found in the vectors passed from the given data segment, using
+        the given Bloom Filter.
+
+        :param data_segment: The segment of EEG feature data vectors to check.
+        :param bloom_filter: The Bloom Filter to use to check the vectors.
+        :returns: The number of matching elements.
+        """
+        hits = 0
+        for vector in data_segment:
+            new_hits = cls._check_vector_against_filter(vector, bloom_filter)
+            hits += new_hits
+        return hits
+
+    @staticmethod
+    def _check_vector_against_filter(vector: np.ndarray, bloom_filter: rbloom.Bloom) -> int:
+        """
+        Accumulates the number of matching elements are found in the given feature data vector, using the given
+        Bloom Filter.
+
+        :param vector: The feature data vector to check.
+        :param bloom_filter: The Bloom Filter to use to check the vector.
+        :returns: The number of matching elements from the vector.
+        """
+        hits = 0
+        for element in vector:
+            if element in bloom_filter:
+                hits += 1
+        return hits
