b30ca097d3b5475a2941da38838a92129df3ca86
Cristopher Hernandez <22552070+CristopherH95@users.noreply.github.com>
2024-04-06 01:17:12 -0700

    Add unit test for column comparison

--- tests/test_comparison.py
+++ tests/test_comparison.py
@@ -46,6 +46,21 @@ class EEGTemplateDataCheckerTestCase(unittest.TestCase):
 
         self.assertEqual(equality_check.hit_ratio, 0.5)
 
+    def test_semi_equal_column_data(self):
+        # Half equal data is spread column-wise, so a column comparison should find the 50% equality.
+        test_data_a = np.random.rand(10)
+        test_data_b = [
+            np.concatenate((test_data_a[0:3], np.random.rand(2))),
+            np.concatenate((test_data_a[3:5], np.random.rand(3)))
+        ]
+        test_bloom_filters = self._make_test_bloom_filter(test_data_a)
+        test_template = DummyEEGTemplateData(test_bloom_filters, 0.5, row_wise=False)
+        checker = EEGTemplateDataChecker(test_template)
+
+        equality_check = checker.check(test_data_b)
+
+        self.assertEqual(equality_check.hit_ratio, 0.5)
+
     @staticmethod
     def _make_test_bloom_filter(data: np.ndarray) -> typing.List[rbloom.Bloom]:
         bloom_filter = rbloom.Bloom(len(data) * 2, 0.01)
