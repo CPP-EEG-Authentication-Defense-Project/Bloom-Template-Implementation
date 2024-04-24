1fb98d8f0c85abe49c3539b69b4090bde5cca9a6
Cristopher Hernandez <22552070+CristopherH95@users.noreply.github.com>
2024-04-23 22:00:30 -0700

    Implement token backend

---
+++ src/eeg_bloom_template/backend/token_backend.py
@@ -0,0 +1,21 @@
+import auth_token_normalization
+import typing
+import numpy as np
+
+from .base import BaseBloomFilterHashBackend
+from ..utils import number_values
+
+
+class TokenBackend(BaseBloomFilterHashBackend):
+    """
+    Bloom filter backend which uses a token to orthonormalize data and compute pseudo-hash values.
+    """
+    def __init__(self, token: typing.Union[int, str, float]):
+        self._token = token
+        super().__init__()
+
+    def run_hash_function(self, data: bytes) -> int:
+        data_vector = np.array([b for b in data])
+        normalized_vector = auth_token_normalization.normalize_data(data_vector, self._token)
+        data_sum = sum(normalized_vector)
+        return number_values.clamp_value(data_sum, -2**127, 2**127)
