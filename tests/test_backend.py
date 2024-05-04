dadde25ff476ba32e546ac6a58be1480c4642bcc
Cristopher Hernandez <22552070+CristopherH95@users.noreply.github.com>
2024-05-04 00:06:22 -0700

    Update unit tests

--- tests/test_backend.py
+++ tests/test_backend.py
@@ -6,7 +6,7 @@ from eeg_bloom_template.backend.mmh3_backend import MMH3BloomFilterBackend
 
 class BackendTestCase(unittest.TestCase):
     def test_fnv_backend(self):
-        test_value = 42
+        test_value = 42.1
         expected = 140121297216442123210317922797979092383
         backend = FNVBloomFilterBackend()
 
@@ -15,7 +15,7 @@ class BackendTestCase(unittest.TestCase):
         self.assertEqual(hashed_value, expected)
 
     def test_mmh3_backend(self):
-        test_value = 42
+        test_value = 42.1
         expected = 125910445295375924307876186304187122577
         backend = MMH3BloomFilterBackend()
 
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,16 @@
 import unittest
+import string
+import random
+import numpy as np
 
 from eeg_bloom_template.utils.iteration import iter_ratio_slices
 from eeg_bloom_template.utils.number_values import convert_unsigned_128_to_signed
+from eeg_bloom_template.utils.orthonormalization import TokenDataGenerator, TokenMatrixNormalization
+
+
+class DummyTokenDataGenerator(TokenDataGenerator):
+    def generate_matrix(self, dimension: int) -> np.ndarray:
+        return np.array([[2] * dimension] * dimension)
 
 
 class UtilsTestCase(unittest.TestCase):
@@ -25,3 +34,40 @@ class UtilsTestCase(unittest.TestCase):
         actual = convert_unsigned_128_to_signed(max_signed_128)
 
         self.assertEqual(max_signed_128, actual)
+
+    def test_orthonormalization_method(self):
+        test_data = np.ones((2,))
+        generator = DummyTokenDataGenerator('fake')
+        normalizer = TokenMatrixNormalization(generator)
+        expected_output = np.array([4, 4])
+
+        normalized_data = normalizer.normalize(test_data)
+
+        self.assertEqual(len(normalized_data), len(expected_output))
+        for actual, expected in zip(normalized_data, expected_output):
+            self.assertEqual(actual, expected)
+
+    def test_invalid_orthonormalization_mixing(self):
+        test_data = np.ones((10,))
+        matrix_data = np.ones((5, 5))
+
+        def run_mixing():
+            return TokenMatrixNormalization.mix_token_matrix(test_data, matrix_data)
+
+        self.assertRaises(ValueError, run_mixing)
+
+    def test_random_token_generation(self):
+        test_size = 256
+        token = TokenDataGenerator.generate_random_token(size=test_size)
+
+        self.assertIsInstance(token, str)
+        # The hex token generation doubles the size used, so we expect the token to be double the given size.
+        self.assertEqual(len(token), test_size * 2)
+
+    def test_random_matrix_generation(self):
+        token = ''.join(random.choice(string.ascii_lowercase) for _ in range(32))
+        generator = TokenDataGenerator(token)
+
+        matrix = generator.generate_matrix(4)
+
+        self.assertEqual(matrix.shape, (4, 4))
