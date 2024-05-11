c0dccdd1b95f99cd5f96ad7c062582d820ef0304
Cristopher Hernandez <22552070+CristopherH95@users.noreply.github.com>
2024-05-10 19:04:39 -0700

    Add unit test for cache flag

--- tests/test_backend.py
+++ tests/test_backend.py
@@ -37,3 +37,23 @@ class BackendTestCase(unittest.TestCase):
             hashed_value = backend.hash_data(test_value)
 
         self.assertEqual(hashed_value, expected)
+
+    def test_token_backend_cache_flag(self):
+        test_value = 42.1
+        test_vector = [data for data in struct.pack('f', test_value)]
+        cached_path = 'eeg_bloom_template.utils.orthonormalization.normalize_cached'
+        normalizer_path = 'eeg_bloom_template.utils.orthonormalization.TokenMatrixNormalization'
+
+        with unittest.mock.patch(cached_path) as fake_normalize:
+            fake_normalize.return_value = test_vector
+            backend = TokenBackend('fake', use_cache=True)
+            backend.run_hash_function(b'1')
+            cached_called = fake_normalize.called
+        with unittest.mock.patch(normalizer_path) as fake_normalizer:
+            fake_normalizer.return_value.normalize.return_value = test_vector
+            backend = TokenBackend('fake', use_cache=False)
+            backend.run_hash_function(b'1')
+            normalizer_called = fake_normalizer.called
+
+        self.assertTrue(cached_called)
+        self.assertTrue(normalizer_called)
