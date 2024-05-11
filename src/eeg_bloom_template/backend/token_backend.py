0899a3951057bbf45ddf8ebcd3b677fa60f83f06
Cristopher Hernandez <22552070+CristopherH95@users.noreply.github.com>
2024-05-10 18:58:24 -0700

    Add flag controlling whether to use cache

--- src/eeg_bloom_template/backend/token_backend.py
+++ src/eeg_bloom_template/backend/token_backend.py
@@ -9,13 +9,19 @@ class TokenBackend(BaseBloomFilterHashBackend):
     """
     Bloom filter backend which uses a token to orthonormalize data and compute pseudo-hash values.
     """
-    def __init__(self, token: typing.Union[int, str, float]):
+    def __init__(self, token: typing.Union[int, str, float], use_cache=True):
         self._token = token
+        self._use_cache = use_cache
         super().__init__()
 
     def run_hash_function(self, data: bytes) -> int:
         data_vector = np.array([b for b in data])
-        normalized_vector = orthonormalization.normalize_cached(self._token, data_vector)
+        if self._use_cache:
+            normalized_vector = orthonormalization.normalize_cached(self._token, data_vector)
+        else:
+            generator = orthonormalization.TokenDataGenerator(self._token)
+            normalizer = orthonormalization.TokenMatrixNormalization(generator)
+            normalized_vector = normalizer.normalize(data_vector)
         data_sum = np.sum(normalized_vector)
         clamped_sum = number_values.clamp_value(data_sum, -2**127, 2**127)
         # Ensure an int is returned
