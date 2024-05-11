4ea64d43a60ab284dda72f6837f0eb7eaca394c1
Cristopher Hernandez <22552070+CristopherH95@users.noreply.github.com>
2024-05-10 17:38:29 -0700

    Add token backend to top level import

--- src/eeg_bloom_template/backend/__init__.py
+++ src/eeg_bloom_template/backend/__init__.py
@@ -1,3 +1,4 @@
 from .base import BaseBloomFilterHashBackend
 from .fnv_backend import FNVBloomFilterBackend
 from .mmh3_backend import MMH3BloomFilterBackend
+from .token_backend import TokenBackend
